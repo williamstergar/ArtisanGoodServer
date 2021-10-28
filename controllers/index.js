1e8494309fd000e8e72ed890b651685d18fac39a
Keegan Snell <Klsnell0351@gmail.com>
2021-10-28 16:32:22 -0400

    got database connected, but still get 'failed to register user' in postman

--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,4 @@
 module.exports={
     userController:require('./usercontroller'),
-    artisanItemController:require('/artisanitemcontroller')
+   
 }
--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 const { UserModel } = require('../models');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
 const { UniqueConstraintError } = require('sequelize/lib/errors');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
 router.post("/register", async (req, res) => {
     let{email, password} = req.body.user;
