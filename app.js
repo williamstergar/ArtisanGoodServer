3374f49c6c1f80c9e3ea62c468998c7ab25dcef3
klsnell <89852835+klsnell@users.noreply.github.com>
2021-10-28 22:04:11 -0400

    Revert "Keegan"

--- app.js
+++ app.js
@@ -1,21 +1,6 @@
-require("dotenv").config();
 const Express = require('express');
 const app = Express();
-const dbConnection = require("./db");
-
-app.use(Express.json());
  
-const controllers = require("./controllers");
-
-app.use("/user", controllers.userController);
-
-dbConnection.authenticate()
-    .then(() => dbConnection.sync()) //=> {force: true} {alter: true}
-    .then(() => {
-        app.listen(3000, () =>{
-            console.log(`[Server]: App is listening on 3000.`);
-        });
-    })
-    .catch((err) => {
-        console.log(`[Server]: Server crashed. Error =${err}`);
-    })
+app.listen(3000, () => {
+console.log(`[Server]: App is listening on 3000.`);
+});
--- db.js
+++
@@ -1,5 +0,0 @@
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize("postgres://postgres:Password@localhost:5432/ArtisanGoodServer");
-
-module.exports = sequelize;
